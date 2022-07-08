11a55c398d1caff42b3c6ac22f0b14dd4e9d22a3
Ben Beshara <code@benbeshara.id.au>
2022-07-08 13:00:24 +1000

    Added Release build mode; executable is still over 2mb(!)

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main() {
   while (!WindowShouldClose()) {
     BeginDrawing();
     
-    ClearBackground(RAYWHITE);
+    ClearBackground(WHITE);
     
     store->getState()->GameLoop();
     
